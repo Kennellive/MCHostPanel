13630a85ed554248780ecacf0df1e177c0a847ae
SlastyonArtyom <43656118+SlastyonArtyom@users.noreply.github.com>
2020-05-28 23:05:24 +0300

    Update config-sample.php

--- data/config-sample.php
+++ data/config-sample.php
@@ -11,8 +11,8 @@ define('KT_UPDATE_URL_MC','http://s3.amazonaws.com/MinecraftDownload/launcher/mi
 define('KT_UPDATE_URL_CB','http://dl.bukkit.org/latest-rb/craftbukkit.jar');
 
 // Screen commands (these should never be modified)
-define('KT_SCREEN_CMD_START','/opt/app-root/miniconda3/bin/tmux new -d -s %s /opt/app-root/miniconda3/bin/java -Xincgc -Xms%sM -Xmx%sM -jar craftbukkit.jar');
-define('KT_SCREEN_CMD_EXEC','/opt/app-root/miniconda3/bin/tmux send -t %s "%s" ENTER');
+define('KT_SCREEN_CMD_START','/opt/app-root/miniconda3/bin/tmux -u new -d -s %s /opt/app-root/miniconda3/bin/java -Xincgc -Xms%sM -Xmx%sM -jar craftbukkit.jar');
+define('KT_SCREEN_CMD_EXEC','/opt/app-root/miniconda3/bin/tmux -u send -t %s "%s" ENTER');
 define('KT_SCREEN_CMD_KILL','/opt/app-root/miniconda3/bin/tmux kill-session -t %s');
 define('KT_SCREEN_CMD_KILLALL','killall /opt/app-root/miniconda3/bin/tmux');
 define('KT_SCREEN_CMD_KILLALL_USER','for session in $(/opt/app-root/miniconda3/bin/tmux ls | /bin/grep -o \'[0-9]*\\.%s\'); do /opt/app-root/miniconda3/bin/tmux kill-session -t "${session}"; done');
